7ff138ab5ce8d3cafd24d2f0ca248d1e199064c9
Nikolas-vl <nikvv02@gmail.com>
2025-08-25 20:02:53 +0200

    fixed reviews styles

--- src/components/Camper/Reviews/Reviews.jsx
+++ src/components/Camper/Reviews/Reviews.jsx
@@ -1,11 +1,6 @@
 import Star from '../../Star/Star';
 import s from './Reviews.module.css';
 
-const Stars = ({ value = 0 }) => {
-  const stars = Array.from({ length: 5 }, (_, i) => (i < value ? '★' : '☆'));
-  return <span className={s.stars}>{stars.join(' ')}</span>;
-};
-
 const Reviews = ({ reviews = [] }) => {
   if (!reviews.length) return <p>No reviews yet.</p>;
 
@@ -13,14 +8,14 @@ const Reviews = ({ reviews = [] }) => {
     <ul className={s.list}>
       {reviews.map((r, idx) => (
         <li key={idx} className={s.item}>
-          <div className={s.avatar}>{r.reviewer_name?.[0] || 'U'}</div>
-          <div>
-            <div className={s.head}>
-              <strong>{r.reviewer_name}</strong>
+          <div className={s.top}>
+            <div className={s.avatar}>{r.reviewer_name?.[0] || 'U'}</div>
+            <div className={s.info}>
+              <strong className={s.name}>{r.reviewer_name}</strong>
               <Star value={Number(r.reviewer_rating) || 0} mode="multi" />
             </div>
-            <p>{r.comment}</p>
           </div>
+          <p className={s.comment}>{r.comment}</p>
         </li>
       ))}
     </ul>
